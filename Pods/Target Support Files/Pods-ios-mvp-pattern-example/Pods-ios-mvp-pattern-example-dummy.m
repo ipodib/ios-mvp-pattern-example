a9d7f77b476a210473fae78a6e43272055a96c7e
Ivan Podibka <ivanpodibka@Ivans-MacBook-Pro.local>
2018-08-10 01:33:49 +0300

    Created base project with Alamofire libraries.

---
+++ Pods/Target Support Files/Pods-ios-mvp-pattern-example/Pods-ios-mvp-pattern-example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_mvp_pattern_example : NSObject
+@end
+@implementation PodsDummy_Pods_ios_mvp_pattern_example
+@end
---
+++ ios-mvp-pattern-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-mvp-pattern-example
+//
+//  Created by Ivan Podibka on 10/08/2018.
+//  Copyright © 2018 Ivan Podibka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
